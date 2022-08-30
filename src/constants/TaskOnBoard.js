72815d4a334832b94b7dfb6a5d627d8ccd7996c4
dvärgen <dvargen@MacBook-Air-som-tillhor-angelica.local>
2022-08-30 12:54:47 +0200

    add functionality to PirateLastName.js, TaskOnBoard.js

---
+++ src/constants/TaskOnBoard.js
@@ -0,0 +1,16 @@
+const TaskOnBoard = [ 
+    "SHARK-BAIT", 
+    "CANNONBALL", 
+    "CAPTAIN", 
+    "PLANKWALKER", 
+    "QUARTERMASTER",
+    "MASTER GUNNER",
+    "BOATSWAIN",
+    "CABIN BOY",
+    "POWDER MONKEY",
+    "SWABBIE",
+    "SAILING MASTER"
+    ]
+
+
+export default TaskOnBoard
